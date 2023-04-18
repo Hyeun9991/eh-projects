ca366a580ebdca8c684909d420013b34b24d5ed1
Hyeun9991 <hyeun9991@gmail.com>
2023-04-18 16:48:34 +0900

    입력form 수정

--- eh-todolist/src/components/InsertItem.tsx
+++ eh-todolist/src/components/InsertItem.tsx
@@ -27,7 +27,7 @@ const InsertItem = ({ items, setItems }: Props) => {
         type="text"
         value={itemName}
         onChange={changeItemName}
-        className="bg-cs-gray-02/60 text-cs-dark-gray placeholder:text-cs-dark-gray/60 w-5/6 px-3 py-1.5 text-sm rounded-lg outline-none focus:ring-4 ring-cs-gray-03/50"
+        className="bg-cs-gray-02/60 text-cs-dark-gray placeholder:text-cs-dark-gray/60 w-[85%] px-3 py-1.5 text-sm rounded-lg outline-none focus:ring-4 ring-cs-gray-03/50"
       />
       <button
         onClick={clickButton}
